4fd7cfe09b7329d01c1ab05d0374e468d9a766d2
zhanostapko <zans.ostapko@gmail.com>
2025-05-21 10:49:19 +0300

    change middleware

--- middleware.ts
+++ middleware.ts
@@ -6,9 +6,9 @@ export async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
   const { pathname } = req.nextUrl;
 
-  if (!token && (pathname.startsWith("/auth") || pathname.startsWith("/api"))) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
+  // if (!token && (pathname.startsWith("/auth") || pathname.startsWith("/api"))) {
+  //   return NextResponse.redirect(new URL("/login", req.url));
+  // }
 
   if (token && pathname === "/login") {
     return NextResponse.redirect(new URL("/auth", req.url));
